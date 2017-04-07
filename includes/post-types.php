13a13eb495b773e3460c8867d844eca16c45e348
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-04-07 10:46:11 +0700

    add post types

--- includes/post-types.php
+++ includes/post-types.php
@@ -12,7 +12,7 @@ function setup() {
 	};
 
 	// NOTE: Uncomment to activate post type
-	// add_action( 'init', $n( 'register_my_post_type' ) );
+	add_action( 'init', $n( 'register_my_post_types' ) );
 
 }
 
@@ -22,9 +22,26 @@ function setup() {
  * See https://github.com/johnbillion/extended-cpts for more information
  * on registering post types with the extended-cpts library.
  */
-function register_my_post_type() {
-	register_extended_post_type( 'my_post_type', array(
-		'menu_icon' 		=> 'dashicons-layout',
+function register_my_post_types() {
+	
+	register_extended_post_type( 'project', array(
+		'menu_icon' 		=> 'dashicons-media-spreadsheet',
 		'supports' 			=> array( 'title', 'editor', 'excerpt', 'thumbnail' ),
 	) );
+
+	register_extended_post_type( 'team_member', array(
+		'menu_icon' 		=> 'dashicons-businessman',
+		'supports' 			=> array( 'title', 'editor', 'excerpt', 'thumbnail' ),
+	) );
+
+	register_extended_post_type( 'event', array(
+		'menu_icon' 		=> 'dashicons-calendar-alt',
+		'supports' 			=> array( 'title', 'editor', 'excerpt', 'thumbnail' ),
+	) );
+
+	register_extended_post_type( 'publication', array(
+		'menu_icon' 		=> 'dashicons-book-alt',
+		'supports' 			=> array( 'title', 'editor', 'excerpt', 'thumbnail' ),
+	) );
+
 }
