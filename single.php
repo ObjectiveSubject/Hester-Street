1a4332711cdefe87945197a16be6d9df79be6142
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-04-20 14:39:33 +0700

    build out single news template

--- single.php
+++ single.php
@@ -3,6 +3,7 @@
  * General page template
  */
 
+global $post;
 get_header(); ?>
 
 	<div class="site-content">
@@ -26,24 +27,61 @@ get_header(); ?>
                                 </div>
 
                                 <div class="section__content flex__item">
-
-                                    <?php if ( get_the_post_thumbnail() ) : ?>
-
-                                    <div class="post-image" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);"></div>
-
-                                    <?php endif; ?>
                                     
-                                    <h1 class="post-title u-mt-pull">
+                                    <div class="h2 u-mt-pull"><?php _e( 'News', 'hsc' ); ?></div>
+
+                                    <h1 class="post-title u-mt-3">
                                         <?php the_title(); ?>
                                     </h1>
 
-                                    <div class="post-content">
+                                    <h2 class="h3 u-mt-0"><?php the_date(); ?></h2>
+
+                                     <?php if ( get_the_post_thumbnail() ) : ?>
+                                        <div class="post-image u-mt-3" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);"></div>
+                                    <?php endif; ?>
+
+                                    <div class="post-content u-mt-2">
                                         <?php the_content(); ?>
                                     </div>
 
                                 </div>
 
                             </div><!-- .flex -->
+                        </div><!-- .u-container -->
+                    </section>
+
+                    <!--Related Posts-->
+                    <section class="section">
+                        <div class="flex u-container">
+
+                            <div class="section__content flex__item u-width-12">
+                                
+                                <div class="h6 u-mt-0"><?php _e( 'Recent News', 'hsc' ); ?></div>
+
+                                <?php 
+                                $args = array(
+                                    'posts_per_page' => 2,
+                                    'post__not_in' => array( $post->ID ),
+                                );
+                                $recent = new WP_Query( $args ); ?>
+
+                                <?php if ( $recent->have_posts() ) : ?>
+                                    <ul class="u-clearfix">
+                                        <?php while( $recent->have_posts() ) : $recent->the_post(); ?>
+                                            <li class="u-span-6">
+                                                <a href="<?php the_permalink(); ?>" class="u-display-block">
+                                                    <?php if ( get_the_post_thumbnail() ) : ?>
+                                                        <div class="post-image" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>);"></div>
+                                                    <?php endif; ?>
+                                                    <h3 class="h5"><?php the_title(); ?></h3>
+                                                </a>
+                                            </li>
+                                        <?php endwhile; ?>
+                                    </ul>
+                                <?php endif; ?>
+
+                            </div>
+
                         </div><!-- .u-container -->
                     </section>
                     
