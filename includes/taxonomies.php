e0db8ce6c4e2ddc62643e9ab5fae05b34580974d
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-04-21 16:31:05 +0700

    add taxos

--- includes/taxonomies.php
+++ includes/taxonomies.php
@@ -12,7 +12,7 @@ function setup() {
 	};
 
 	// NOTE: Uncomment to activate taxonomy
-	//add_action( 'init', $n( 'register_taxonomy' ) );
+	add_action( 'init', $n( 'register_taxonomies' ) );
 
 }
 
@@ -21,6 +21,22 @@ function setup() {
  *
  * See https://github.com/johnbillion/extended-taxos for more info on using the extended-taxos library
  */
-function register_taxonomy() {
-	register_extended_taxonomy( 'my_taxo', 'post' );
+function register_taxonomies() {
+
+	register_extended_taxonomy( 'service', array('project', 'publication') );
+	register_extended_taxonomy( 'issue', array('project', 'publication') );
+	register_extended_taxonomy( 'location', array('project') );
+	register_extended_taxonomy( 'status', array('project'), array(), array(
+		# Override the base names used for labels:
+		'singular' => 'Status',
+		'plural'   => 'Statuses',
+		'slug'     => 'statuses'
+	) );
+	register_extended_taxonomy( 'team_role', array('team_member'), array(), array(
+		# Override the base names used for labels:
+		'singular' => 'Role',
+		'plural'   => 'Roles',
+		'slug'     => 'roles'
+	) );
+
 }
