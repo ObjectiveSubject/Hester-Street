53d613cf8ca02c0fa988f5308c3f93f7e616e4a3
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-05-09 09:58:37 +0700

    silo project sidebar into own file

--- single-project.php
+++ single-project.php
@@ -11,13 +11,6 @@ $end_date = get_post_meta($post->ID, 'project_end_date', true);
 $date_string = array();
 if ( $begin_date ) $date_string[] = date( 'Y', $begin_date );
 $date_string[] = ( $end_date ) ? date( 'Y', $end_date ) : 'Present';
-$services = get_the_terms( $post->ID, 'service' );
-$issues = get_the_terms( $post->ID, 'issue' );
-$team_members = get_post_meta( $post->ID, 'project_team_members', true );
-$collaborators = get_post_meta( $post->ID, 'project_collaborators', true );
-$partners = get_post_meta( $post->ID, 'project_partners', true );
-$site = get_post_meta( $post->ID, 'project_site', true );
-$site_url = get_post_meta( $post->ID, 'project_site_url', true );
 
 get_header(); ?>
 
@@ -84,82 +77,9 @@ get_header(); ?>
                                             <div class="h6 u-mt-0">Description</div>
                                             <?php the_content(); ?>
                                         </div>
-                                        <ul class="project-sidebar">
-                                            
-                                            <?php if ( $services ) : ?>
-                                            <li class="project-sidebar__group u-mb-3 u-mt-0">
-                                                <div class="h6 u-mt-0">Scope</div>
-                                                <ul class="u-mt-1">
-                                                    <?php foreach( $services as $service ) : ?>
-                                                        <li><?php echo $service->name; ?></li>
-                                                    <?php endforeach; ?>
-                                                </ul>
-                                            </li>
-                                            <?php endif; ?>
-
-                                            <?php if ( $issues ) : ?>
-                                            <li class="project-sidebar__group u-mb-3 u-mt-0">
-                                                <div class="h6 u-mt-0">Issues</div>
-                                                <ul class="u-mt-1">
-                                                    <?php foreach( $issues as $issue ) : ?>
-                                                        <li><?php echo $issue->name; ?></li>
-                                                    <?php endforeach; ?>
-                                                </ul>
-                                            </li>
-                                            <?php endif; ?>
-
-                                            <?php if ( $team_members ) : ?>
-                                            <li class="project-sidebar__group u-mb-3 u-mt-0">
-                                                <div class="h6 u-mt-0">Team Members</div>
-                                                <ul class="u-mt-1">
-                                                    <?php foreach ( $team_members as $member ) : ?>
-                                                        <li><a href="<?php echo get_permalink($member); ?>" class="u-color-hover-green"><?php echo get_the_title( $member ); ?></a></li>
-                                                    <?php endforeach; ?>
-                                                </ul>
-                                            </li>
-                                            <?php endif; ?>
-
-                                            <?php if ( $collaborators ) : ?>
-                                            <li class="project-sidebar__group u-mb-3 u-mt-0">
-                                                <div class="h6 u-mt-0">Collaborators</div>
-                                                <ul class="u-mt-1">
-                                                    <?php foreach ( $collaborators as $collaborator ) : ?>
-                                                        <li><a href="<?php echo get_permalink($collaborator); ?>" class="u-color-hover-green"><?php echo get_the_title( $collaborator ); ?></a></li>
-                                                    <?php endforeach; ?>
-                                                </ul>
-                                            </li>
-                                            <?php endif; ?>
-
-                                            <?php if ( $partners ) : ?>
-                                            <li class="project-sidebar__group u-mb-3 u-mt-0">
-                                                <div class="h6 u-mt-0">Partners</div>
-                                                <ul class="u-mt-1">
-                                                    <?php foreach ( $partners as $partner ) : 
-                                                        $link_action = get_post_meta( $partner, 'member_link_action', true );
-                                                        $website = get_post_meta( $partner, 'member_website', true );
-                                                        $target = ( $link_action ) ? '_blank' : '_self';
-                                                        $url = ( $link_action ) ? esc_url( $website ) : get_permalink($partner); ?>
-                                                        <li>
-                                                            <a href="<?php echo $url; ?>" target="<?php echo $target; ?>" class="u-color-hover-green">
-                                                                <?php echo get_the_title( $partner ); ?>
-                                                                <?php echo ( $link_action ) ? '&nearr;' : ''; ?>
-                                                            </a>
-                                                        </li>
-                                                    <?php endforeach; ?>
-                                                </ul>
-                                            </li>
-                                            <?php endif; ?>
-
-                                            <?php if ( $site && $site_url ) : ?>
-                                            <li class="project-sidebar__group u-mb-3 u-mt-0">
-                                                <div class="h6">Project Site</div>
-                                                <ul class="u-mt-1">
-                                                    <li><a href="<?php echo esc_url($site_url); ?>" target="_blank" class="u-color-hover-green"><?php echo $site . ' &nearr;'; ?></a></li>
-                                                </ul>
-                                            </li>
-                                            <?php endif; ?>
-
-                                        </ul>
+                                        <div class="project-sidebar">
+                                            <?php get_sidebar( 'project' ); ?>
+                                        </div>
                                     </div>
 
                                 </div>
