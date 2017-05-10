83477a49fce4f21506a98ec18ee58520ae5bfe69
Kirk Pettinga <kirk.pettinga@gmail.com>
2017-05-10 14:56:41 +0700

    remove CMB2 in favor of ACF

--- functions.php
+++ functions.php
@@ -16,11 +16,9 @@ define( 'HSC_ASSETS',       HSC_TEMPLATE_URL . '/assets/' );
 // Include compartmentalized functions
 require_once HSC_INC . 'core.php';
 
-require_once HSC_INC . 'cmb2.addons.php';
 require_once HSC_INC . 'api.php';
 require_once HSC_INC . 'comments.php';
 require_once HSC_INC . 'helpers.php';
-require_once HSC_INC . 'metaboxes.php';
 require_once HSC_INC . 'post-types.php';
 require_once HSC_INC . 'shortcodes.php';
 require_once HSC_INC . 'taxonomies.php';
@@ -29,9 +27,6 @@ require_once HSC_INC . 'template-tags.php';
 // Include lib classes
 include( HSC_INC . 'libraries/extended-cpts.php' );
 include( HSC_INC . 'libraries/extended-taxos.php' );
-include( HSC_INC . 'libraries/cmb2/init.php' );
-include( HSC_INC . 'libraries/cmb2-attached-posts/cmb2-attached-posts-field.php' );
-include( HSC_INC . 'libraries/cmb2-post-search-field/cmb2_post_search_field.php' );
 
 // Run the setup functions
 HSC\Core\setup();
